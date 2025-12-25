f9c69b4dd913e70a161b65ba56aed11770205e74
MarcioUfs <devil@academico.ufs.br>
2025-12-25 20:11:31 -0300

    Refactor(dev):Modificacao de rota de botao signup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -57,7 +57,7 @@ export default function Home() {
                   Login
                 </button>
               </Link>
-              <Link href="/signup">
+              <Link href="/usuario/criar">
                 <button className="px-4 py-2 bg-blue-600 text-white rounded-md hover:bg-blue-700 transition-colors">
                   Sign Up
                 </button>
